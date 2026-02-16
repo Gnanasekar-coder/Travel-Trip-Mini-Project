bdd502ae7200f0d86aa1f6cc821cb0fc094161a7
gnanasekar boggala <gnanasekhar271@gmail.com>
2026-02-16 12:24:10 +0000

    first time

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -50,7 +50,89 @@ class Login extends Component {
       const jwtToken = fetchedData.jwt_token
 
       Cookies.set('jwt_token', jwtToken, {expires: 30})
+
+      const {history} = this.props
+      history.replace('/')
+    } else {
+      const fetchedData = await response.json()
+
+      this.setState({
+        isSubmitError: true,
+        errorMessage: fetchedData.error_msg,
+      })
     }
+  }
+
+  render() {
+    const jwtToken = Cookies.get('jwt_token')
+
+    if (jwtToken !== undefined) {
+      return <Redirect to="/" />
+    }
+
+    const {
+      username,
+      password,
+      isShowPassword,
+      errorMessage,
+      isSubmitError,
+    } = this.state
+
+    return (
+      <div className="login-container">
+        <div className="login-card">
+          <h1 className="login-website-logo">Travel Trip</h1>
+          <form onSubmit={this.onSubmitLoginForm}>
+            <div className="username-input-container">
+              <label className="label" htmlFor="username">
+                Username
+              </label>
+              <br />
+              <input
+                type="text"
+                id="username"
+                className="username-input"
+                placeholder="Username"
+                onChange={this.onChangeUsername}
+                value={username}
+              />
+            </div>
+            <div className="password-input-container">
+              <label className="label" htmlFor="password">
+                Password
+              </label>
+              <br />
+              <div className="password-show-hide-container">
+                <input
+                  type={isShowPassword ? 'text' : 'password'}
+                  id="password"
+                  className="password-input"
+                  placeholder="Password"
+                  onChange={this.onChangePassword}
+                  value={password}
+                />
+                <button
+                  type="button"
+                  className="show-password-button"
+                  onClick={this.onToggleShowPassword}
+                  data-testid="show-password"
+                >
+                  {isShowPassword ? (
+                    <BiHide size="20" color="#647488" />
+                  ) : (
+                    <BiShow size="20" color="#647488" />
+                  )}
+                </button>
+              </div>
+            </div>
+            {isSubmitError && <p className="error-message">{errorMessage}</p>}
+            <button type="submit" className="login-button">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    )
   }
 }
 
